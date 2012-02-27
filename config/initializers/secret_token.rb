6f5b0ea55226511bc6a7c06dd92d270778abb675
Javier Alvarez <jalvarezsamayoa@gmail.com>
2012-02-26 20:28:52 -0600

    Carga Inicial Rails 3.2.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OwDenuncias::Application.config.secret_token = '34b24a0ed8d7ee293f2b104d969fdfc11276468912d5ddb8d51515b03b927a95be0fad40a8e0e753ce1a83f03e0d5cbeaecc2ee14558f84e59ef2f0155733254'
